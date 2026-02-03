b99370e394069f6e31e7a715c59cc6b954949fa1
Your Name <your.email@example.com>
2026-02-03 10:52:07 +0530

    practice

--- main.tf
+++ main.tf
@@ -4,7 +4,10 @@ module "vpc" {
     vpc_cidr = var.vpc_cidr
     project_name = var.project_name
     environment = var.environment #varab.tf in terr-aws-folder..we declared these are 
-    vpc_tags = var.vpc_tags           #mandatory and vpc tags optionals so didnt mention tags
+    vpc_tags = var.vpc_tags      
+    public_subnet_cidr= var.public_subnet_cidr  #mandatory and vpc tags optionals so didnt mention tags
+    private_subnet_cidr= var.private_subnet_cidr
+    database_subnet_cidr= var.database_subnet_cidr
 }
 
 
